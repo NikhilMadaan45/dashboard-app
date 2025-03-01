ec966ea1f62c701f02538eeb0ac83acffbb68b08
NikhilMadaan45 <nikhilmadaan45@gmail.com>
2025-03-01 12:54:57 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
